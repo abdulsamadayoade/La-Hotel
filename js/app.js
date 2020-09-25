7e0bb3a760be225bb791dfdac6bd18b36f17be86
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2020-09-25 06:50:55 +0100

    add codes for mobile navigation menu

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// TESTIMONIAL SLIDER
 $('.slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -5,4 +6,15 @@ $('.slider').slick({
     autoplaySpeed: 2000,
     fade: true,
     arrows: false
+});
+
+// MOBILE NAVIGATION
+
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav-list');
+const links = document.querySelectorAll('.nav-list li');
+
+hamburger.addEventListener("click", () => {
+    navList.classList.toggle("open");
+    hamburger.classList.toggle("open");
 });
