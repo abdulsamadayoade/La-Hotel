f9124d8b49a97853751d4e2c59aaca4ef9ef4317
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2020-09-30 01:25:09 +0100

    update app.js

--- js/app.js
+++ js/app.js
@@ -7,14 +7,3 @@ $('.slider').slick({
     fade: true,
     arrows: false
 });
-
-// MOBILE NAVIGATION
-
-const hamburger = document.querySelector('.hamburger');
-const navList = document.querySelector('.nav-list');
-const links = document.querySelectorAll('.nav-list li');
-
-hamburger.addEventListener("click", () => {
-    navList.classList.toggle("open");
-    hamburger.classList.toggle("open");
-});
