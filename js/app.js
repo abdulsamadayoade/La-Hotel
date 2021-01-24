984be5790960893349218bf4bd8a96256d09c8d0
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-01-24 18:07:35 +0100

    Updated

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+// CLOSE THE NAV WHEN NAVLNKS ARE CLICKED (MOBILE ONLY)
+let navList = document.querySelector('.nav-list');
+let navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(function (navLink) {
+    navLink.addEventListener('click', function () {
+        navList.style.left = '-100%';
+    })
+});
+
 // TESTIMONIAL SLIDER
 $('.slider').slick({
     slidesToShow: 1,
